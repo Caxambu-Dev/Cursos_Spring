cdde5e11b89dc7a4bb341ca6f13a216279ed2abe
Daniel Caxambu <daniel_caxambu@live.com>
2022-09-03 10:21:04 -0300

    Criando Servlet 'NovaEmpresaServlet' e implementando a classe 'service'

---
+++ 0001-Java_Servlet_programacao_web_Java/gerenciador/src/main/java/br/com/alura/gerenciador/servlet/NovaEmpresaServlet.java
@@ -0,0 +1,21 @@
+package br.com.alura.gerenciador.servlet;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet(urlPatterns = "/noveEmpresa")
+public class NovaEmpresaServlet extends HttpServlet {
+	
+	@Override
+	protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		super.service(req, resp);
+	}
+	
+
+}
